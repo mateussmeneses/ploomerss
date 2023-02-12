434efe11e7d84b374e7695043f18349090a40c3f
Mateus Meneses <mateusmeneses86@gmail.com>
2023-02-12 08:46:29 -0300

    abbr & two characters

---
+++ abbr.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function abbreviation(a, b) {
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const a = readLine();
+
+        const b = readLine();
+
+        const result = abbreviation(a, b);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
